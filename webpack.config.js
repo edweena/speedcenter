e3f5039d03eccaf6fb799c7092a4ff7f26fa3e26
James Pants <james@rds.zone>
2017-12-16 14:52:05 -0500

    first

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+//-----------------------------------------
+//
+// Prod
+//
+//-----------------------------------------
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  devtool: 'cheap-module-source-map',
+  entry: './src/js/index',
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'js/bundle.js'
+  },
+
+  plugins: [new HtmlWebpackPlugin({
+    filename: 'index.html',
+    template: 'src/html/template.html'
+  })],
+
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loaders: ['babel-loader'],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        loaders: ['style-loader', 'css-loader']
+      }
+    ]
+  }
+}
